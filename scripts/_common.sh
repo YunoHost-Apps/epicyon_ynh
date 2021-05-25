a02d07dfe07f8bf2510c8d15cd8e12b3bdb74768
Amo <parrot@localhost.localdomain>
2021-05-25 19:56:03 +0530

    Updated dependencies

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,15 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-socks imagemagick \
-    python3-setuptools \
-    python3-cryptography \
-    python3-dateutil \
-    python3-idna python3-requests \
-    python3-django-timezone-field \
-    libimage-exiftool-perl python3-flake8 \
-    python3-pyqrcode python3-png python3-bandit
-"
+pkg_dependencies="python3-socks imagemagick python3-setuptools python3-cryptography python3-dateutil python3-idna python3-requests python3-flake8 python3-django-timezone-field python3-pyqrcode python3-png python3-bandit libimage-exiftool-perl"
 
 #=================================================
 # PERSONAL HELPERS
